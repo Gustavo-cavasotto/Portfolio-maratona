db3ab36dc1ab78641156fc30d2cd25b324bef95d
Gustavo-cavasotto <gustavocpotrich@gmail.com>
2022-06-05 23:25:28 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var hamburguer = document.querySelector(".hamburguer");
+var content = document.getElementById('container');
+var menuitem = document.querySelector(".menu-item");
+
 
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+
+
+function onScroll(){
+    if (scrollY < 700){
+        seta.classList.add('scroll')
+    }
+
+    else{
+        seta.classList.remove('scroll')
+    }
+
+}
+
+f
+
+
+
 
 //esta dizendo que ao clicar na classe hamburguer, vai executar uma função\\
 // esse função acrescenta a classe para .sidebar.show-menu, que coloca retira a margem do menu, fazendo ele aparecer na tela\\
